78fe6e37d3cc6f033e101cba2a8669e0a653eb1b
Sebastian Reh <sebastian.reh@conekta.com>
2024-04-30 13:04:44 -0300

    Use Femsa production URL

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     map: {
         '*': {
             'femsa': 'https://cdn.conekta.io/js/latest/conekta.js',
-            'femsaCheckout': 'https://pay.stg.digitalfemsa.io/v1.0/js/digitalfemsa-checkout.min.js'
+            'femsaCheckout': 'https://pay.digitalfemsa.io/v1.0/js/digitalfemsa-checkout.min.js'
         }
     }
 };
