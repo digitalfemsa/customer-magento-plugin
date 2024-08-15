68bf0be87e2493c94b7c5cf05f37370f5f5d6ea8
Franklin Carrero <franklin.carrero@conekta.com>
2024-08-15 09:31:56 -0500

    CRD-384 adds intencional error recovery order

--- view/frontend/web/js/view/payment/method-renderer/embedform.js
+++ view/frontend/web/js/view/payment/method-renderer/embedform.js
@@ -294,6 +294,9 @@ define(
                         console.error(error);
                     },
                     onFinalizePayment: function (event) {
+                        self.conektaError("error intencional");
+                        return;
+                        
                         self.iframOrderData(event);
                         self.beforePlaceOrder();
                     },
